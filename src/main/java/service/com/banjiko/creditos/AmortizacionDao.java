ae9211dcdc8c64e4794b51de0662b06af2d6ced8
Malandru <andre.mtzd07@gmail.com>
2019-10-24 09:14:32 -0500

    Correccion de la insercion a la db

--- src/main/java/service/com/banjiko/creditos/AmortizacionDao.java
+++ src/main/java/service/com/banjiko/creditos/AmortizacionDao.java
@@ -14,20 +14,18 @@ public class AmortizacionDao
 
     public void add(AmortizacionDto dto)
     {
-        String sentence = "INSERT INTO amortizacion VALUES('', %s,'%s', %s, %s, %s, %s)";
+        String sentence = "INSERT INTO amortizacion VALUES(0, %s,'%s', %s, %s, %s, %s)";
         String insert = String.format(sentence, dto.getNoAmortizacion(),
         dto.getNoCuenta(), dto.getCapital(), dto.getInteres(),
         dto.getTotal(), dto.getSaldo());
-        System.out.println(insert);
         jdbcTemplate.execute(insert);
     }
 
     public void add(Credito credito)
     {
-        String sentence = "INSERT INTO prestamo VALUES('','%s', %s, %s)";
-        String insert = String.format(sentence, credito.getCuenta(),
+        String sentence = "INSERT INTO prestamo VALUES(0,'%s', %s, %s)";
+        String insert = String.format(sentence, credito.getCuenta().getNoCuenta(),
         credito.getMonto(), credito.getPlazoMeses());
-        System.out.println(insert);
         jdbcTemplate.execute(insert);
     }
 }
